cce3206b9ff2545fe5875bcac2407f3dd923c047
Peng fei <18150152026@gmail.com>
2023-05-29 10:57:27 +0800

    Second time (add some learning renference)

---
+++ 02_Day_Variables_builtin_functions/test02.py
@@ -0,0 +1,38 @@
+# Day2: 30 Days of python programming
+# first_name = 'Pengfei'
+# last_name = 'Ma'
+# full_name = 'Pngfei Ma'
+# country = 'China'
+# city = 'Baoji'
+# age = 28
+# is_marragied = False
+# first_name, last_name, full_name, country,city ,age ,is_marragied ='Pengfei','Ma','Pngfei Ma','China','Baoji', 28,False
+
+# print(first_name)
+# print(is_marragied)
+
+# print(age)
+# print(float(age))
+
+# print (type(first_name))
+
+# print (len (first_name))
+
+# compare_firstname_lastname = len(first_name)-len(last_name)
+
+# print (compare_firstname_lastname)
+
+# num_one, num_two = 5,4
+
+# print(num_one,num_two)
+# first_name = ('what is your first name: ')
+# print(first_name)
+
+# radius = int(input('please type the radius: '))
+
+
+# area_of_circle = 3.14 * radius ** 2
+
+# print(area_of_circle)
+
+help
